01aca986499ba65d8c28a143da725f449a27d821
Jiayi Xu <hopeyi.xu@mail.utoronto.ca>
2017-11-06 15:18:06 -0500

    Add contract model and migration

--- jobmine/db/schema.rb
+++ jobmine/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104181017) do
+ActiveRecord::Schema.define(version: 20171105193352) do
+
+  create_table "contracts", force: :cascade do |t|
+    t.integer "accept_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "name"
