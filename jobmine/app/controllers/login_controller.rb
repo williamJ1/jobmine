1e0c8e829f9be35c2384d7202f9be05f511354c0
Ming En Sheng <max.sheng@mail.utoronto.ca>
2017-11-06 22:18:35 -0500

    added validations for user model

--- jobmine/app/controllers/login_controller.rb
+++ jobmine/app/controllers/login_controller.rb
@@ -8,14 +8,12 @@ class LoginController < ApplicationController
     if @u == nil
       flash[:warning] = 'User does not exist, please sign up'
       redirect_to new_user_path
-
       return
     end
 
     if @u.try(:authenticate, params[:login][:password])
       session[:current_user_id] = @u.id
       redirect_to check_profile_path
-
       return
     else
       flash[:warning] = 'Wrong password'
--- jobmine/app/models/user.rb
+++ jobmine/app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
   has_one :profile
+  #attr_accessor :password
+
+  validates :email, presence: {message: "Please provide email address"}, on: :create
+  validates :email, uniqueness: {message: "This email adress is taken"}, on: :create
+  validates :email, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "%Email format not valid" }, on: :create
+  validates :password, length: {minimum: 2, too_short: "your password is too short"}, on: :create
+  validates :password, length: {maximum: 8, too_long: "Your password is too long"}, on: :create
+
 
   #has_many :reviews
 end
