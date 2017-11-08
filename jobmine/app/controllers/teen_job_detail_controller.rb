ccf8de042d56a50cab10ee9474091cfa624f379e
williamJ1 <chengjie.jiang@mail.utoronto.ca>
2017-11-08 00:40:35 -0500

    add apply button to teen_job_detail to generate contract

--- jobmine/app/controllers/teen_job_detail_controller.rb
+++ jobmine/app/controllers/teen_job_detail_controller.rb
@@ -4,7 +4,9 @@ class TeenJobDetailController < ApplicationController
 
     @job_detail = Job.find_by(id: @job_id)
     @employer_user = @job_detail.profile.user
-
+    #the only way to find teen profile id is through session
+    @cur_user_id = session[:current_user_id]
+    @teen_profile = User.find_by(id: @cur_user_id).profile
 
       #user_profile = Profile.find_by(user_id: @cur_user_id)
   end
