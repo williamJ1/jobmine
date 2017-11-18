0b6db949b74f05aa78a686702e8ab6dc43d2bf34
Jiayi Xu <hopeyi.xu@mail.utoronto.ca>
2017-11-18 15:55:41 -0500

    add contract update controller

--- jobmine/app/controllers/contract_controller.rb
+++ jobmine/app/controllers/contract_controller.rb
@@ -10,6 +10,31 @@ class ContractController < ApplicationController
     end
   end
 
+  def update
+    @contract_need_to_accept = Contract.find_by(job_id: params[:job_id], profile_id: params[:profile_id])
+    if @contract_need_to_accept == nil
+      flash[:error] = 'No contract exist for this job_id and profile_id, please check' 
+      redirect_to jobs_path
+    else 
+      @contract_need_to_accept.accept_status = 2
+      if @contract_need_to_accept.save
+        @contracts_need_to_reject = Contract.where(job_id: params[:job_id]).all
+        @contracts_need_to_reject.accept_status = 1
+        if @contracts_need_to_reject.save
+          flash[:notice] = "Accept successful"
+          redirect_to jobs_path
+        else 
+          flash[:notice] = "Reject others failed"
+          redirect_to jobs_path
+        end
+      else 
+        flash[:notice] = "Accept failed"
+        redirect_to jobs_path
+      end
+    end
+  
+
+
   def contract_params
     params.permit(:job_id, :profile_id, :accept_status)
   end
