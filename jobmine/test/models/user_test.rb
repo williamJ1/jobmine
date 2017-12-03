177339416f616872f820a8b42be6d8efdaef77c8
Zhiwen Cai <zhiwen.cai@mail.utoronto.ca>
2017-12-03 15:56:39 -0500

    adding tester template for rspec

--- jobmine/test/models/user_test.rb
+++ jobmine/test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+test "should not save user without email"
+user= User.new
+#assert_not user.save, "Was able to save user without email"
+assert user.invalid?
 end
