a3f6771dea4e05b506a7be47e11e5ab07335cbb5
potatochips00 <len.wu@mail.utoronto.ca>
2017-12-03 14:05:27 -0500

    Fix give review button if already reviewed
    Only show give review button if review does not exist.

--- jobmine/db/seeds.rb
+++ jobmine/db/seeds.rb
@@ -83,12 +83,12 @@ timeslot4 = Timeslot.create(date_time_begin: start, time_length: 1, approve_stat
 
 #reviews
 review1 = Review.create(rating: 4, title:'title1', body: 'review1', profile_id: teen_profile.id, reviewer: 'emp@e.com',reviewer_id: emp_profile.id, contract_id: contract1.id)
-review2 = Review.create(rating: 4.5, title:'title2', body: 'review2', profile_id: teen_profile_1.id, reviewer: 'emp@e.com',reviewer_id: emp_profile.id, contract_id: contract2.id)
+review2 = Review.create(rating: 4.5, title:'title2', body: 'review2', profile_id: teen_profile_1.id, reviewer: 'emp@e.com',reviewer_id: emp_profile.id, contract_id: contract1.id)
 review3 = Review.create(rating: 3.5, title:'title3', body: 'review3', profile_id: teen_profile_2.id, reviewer: 'emp@e.com',reviewer_id: emp_profile.id, contract_id: contract1.id)
 review4 = Review.create(rating: 5, title:'title4', body: 'review4', profile_id: emp_profile.id, reviewer: 'teen@t.com',reviewer_id: teen_profile.id, contract_id: contract3.id)
 review5 = Review.create(rating: 3, title:'title5', body: 'review5', profile_id: emp_profile.id, reviewer: 'teen@t.com',reviewer_id: teen_profile.id, contract_id: contract3.id)
 review6 = Review.create(rating: 2.5, title:'title6', body: 'review6', profile_id: emp_profile_1.id, reviewer: 'teen2@t.com',reviewer_id: teen_profile_2.id, contract_id: contract4.id)
-review7 = Review.create(rating: 2, title:'title7', body: 'review7', profile_id: teen_profile_1.id, reviewer: 'emp1@e.com',reviewer_id: emp_profile_1.id, contract_id: contract2.id)
+review7 = Review.create(rating: 2, title:'title7', body: 'review7', profile_id: teen_profile_1.id, reviewer: 'emp1@e.com',reviewer_id: emp_profile_1.id, contract_id: contract1.id)
 review8 = Review.create(rating: 1.5, title:'title8', body: 'review8', profile_id: teen_profile_2.id, reviewer: 'emp1@e.com',reviewer_id: emp_profile_1.id, contract_id: contract4.id)
 review9 = Review.create(rating: 1, title:'title9', body: 'review9', profile_id: emp_profile.id, reviewer: 'teen1@t.com',reviewer_id: teen_profile_1.id, contract_id: contract3.id)
 review10 = Review.create(rating: 4.5, title:'title10', body: 'review10', profile_id: emp_profile.id, reviewer: 'teen1@t.com',reviewer_id: teen_profile_1.id, contract_id: contract3.id)
